c9995f466848e9c92d45d4e6d59db7866f55cc4e
raghuaravinthan <108924740+raghuaravinthan@users.noreply.github.com>
2025-07-22 19:39:35 +0530

    Delete togglemenu.js

--- togglemenu.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  document.getElementById("navMenu").classList.toggle("show");
-}
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth >= 769) {
-    document.getElementById("navMenu").classList.remove("show");
-  }
-});
